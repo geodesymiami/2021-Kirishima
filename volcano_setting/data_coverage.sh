ea541bf2adf6171f26a80ca7afbc5bf6125b64e1
Zhang Yunjun <yunjunz@users.noreply.github.com>
2019-09-30 15:00:12 -0400

    update post_sbasApp and GBIS_1

--- volcano_setting/data_coverage.sh
+++ volcano_setting/data_coverage.sh
@@ -10,11 +10,15 @@ OUTFILE='data_coverage.ps'
 
 
 ##------------------------ Background -------------------------##
-gmt pscoast -R129.5/131.9/30.9/33.9 -JM1.8i -Dh -B1wens -Na -Swhite -Gdimgray -P -V -K > $OUTFILE
+gmt pscoast -R129.65/131.8/30.9/33.9 -JM1.6i -Dh -B1wens -Na -Swhite -Gdimgray -P -V -K > $OUTFILE
+#gmt pscoast -R129.5/131.9/30.9/33.9 -JM1.8i -Dh -B1wens -Na -Swhite -Gdimgray -P -V -K > $OUTFILE
 #gmt pscoast -R129.9/131.7/31/32.8 -JM2i -Dh -B1wens -Na -Swhite -Ggray60 -P -V -K > $OUTFILE
 #gmt pscoast -R129.9/131.7/31/32.8 -JM2.5i -Dh -B1wens -Na -Swhite -Ggray55 -P -V -K > $OUTFILE
 #gmt pscoast -R129.9/131.7/31/32.8 -JB131.3/32/31/33/2.5i -Dh -B1wens -Na -Swhite -Ggray55 -P -V -K > $OUTFILE
 
+mplB='31/119/180'
+mplO='255/127/14'
+mplG='44/160/44'
 
 ##------------------------ Data Coverage -------------------------##
 # ALOS-1 asc
@@ -22,7 +26,7 @@ LAT1=31.2525;   LON1=130.5265
 LAT2=31.3676;   LON2=131.2565
 LAT3=32.5389;   LON3=130.2444
 LAT4=32.6548;   LON4=130.9939
-gmt psxy <<END -R -J -Wthin,31/119/180,- -V -O -K >> $OUTFILE
+gmt psxy <<END -R -J -Wthick,$mplG,- -V -O -K >> $OUTFILE
 $LON1   $LAT1
 $LON2   $LAT2
 $LON4   $LAT4
@@ -35,7 +39,7 @@ LAT1=32.5352;   LON1=131.2640
 LAT2=32.6469;   LON2=130.5295
 LAT3=31.2446;   LON3=130.9899
 LAT4=31.3576;   LON4=130.2606
-gmt psxy <<END -R -J -Wthin,31/119/180,- -V -O -K >> $OUTFILE
+gmt psxy <<END -R -J -Wthick,$mplG,- -V -O -K >> $OUTFILE
 $LON1   $LAT1
 $LON2   $LAT2
 $LON4   $LAT4
@@ -48,7 +52,7 @@ LAT1=31.3783;   LON1=130.6764
 LAT2=31.4646;   LON2=131.2634
 LAT3=31.9890;   LON3=130.5543
 LAT4=32.0757;   LON4=131.1493
-gmt psxy <<END -R -J -Wthin,255/127/14,- -V -O -K >> $OUTFILE
+gmt psxy <<END -R -J -Wthick,$mplO,- -V -O -K >> $OUTFILE
 $LON1   $LAT1
 $LON2   $LAT2
 $LON4   $LAT4
@@ -61,7 +65,7 @@ LAT1=32.3278;   LON1=131.1299
 LAT2=32.4164;   LON2=130.5535
 LAT3=31.7154;   LON3=131.0090
 LAT4=31.8060;   LON4=130.4249
-gmt psxy <<END -R -J -Wthin,255/127/14,- -V -O -K >> $OUTFILE
+gmt psxy <<END -R -J -Wthick,$mplO,- -V -O -K >> $OUTFILE
 $LON1   $LAT1
 $LON2   $LAT2
 $LON4   $LAT4
